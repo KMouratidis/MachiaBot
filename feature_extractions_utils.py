18c25de29c06f06c2d74403c89e8f269d0aa8cf5
KMouratidis <k.mouratidis@Hotmail.com>
2018-04-09 00:36:46 +0300

    Some bugfixes and a new function for topic modelling.

--- feature_extractions_utils.py
+++ feature_extractions_utils.py
@@ -1,33 +1,51 @@
 from sqlite_utils import fetch_all
-from string import punctuation
 from collections import Counter, defaultdict
 import numpy as np
 
 from nltk import word_tokenize
 from nltk import WordNetLemmatizer
 from nltk import wordnet as wn
+from nltk.collocations import FreqDist, ngrams
 from nltk.corpus import stopwords
 
 from gensim.utils import simple_preprocess
+
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer
 
 machiavelli, montesquieu = fetch_all()
 
 stop_words = stopwords.words("english")
 
 
+def _basic_assertions(corpus, advanced_assertions=False):
+    """
+    Every subsequent function will use this to check if the input is in the correct format.
+    The argument advanced_assertions is meant for functions that expect a list of lists with string tokens.
+    """
+
+    assert isinstance(corpus, list), "Input `corpus` is not a list."
+
+    # TODO: Should other data types be included for tokens, somehow?
+    if advanced_assertions:
+        assert (all(isinstance(sent, list) for sent in corpus)), "Sentences are not lists."
+        assert (all(isinstance(token, str) for sent in corpus for token in sent)), "Tokens are not strings."
+
+
 def tokenize(text, remove_stopwords=False, remove_punctuation=True, lower=True):
     """
     Helper function to tokenize text with various optional arguments.
 
-    :param text: Should be a list of strings (sentences, documents, etc)
+    :param text: Should be a list of strings (sentences, documents, etc),
+           e.g.: [["Going a trip around town."], ["It is a wonderful experience."]].
     :param remove_stopwords: Remove common words. Using nltk's stopwords for English. Optional, default=False.
-    :param remove_punctuation: Removes punctuation. Optional, default=True
-    :param lower: Lower the text. Optional, default=True
-    :return: Tokenized text, a list of lists with tokens.
+    :param remove_punctuation: Removes punctuation. Optional, default=True.
+    :param lower: Lower the text. Optional, default=True.
+    :return: Tokenized text, a list of lists with tokens, e.g.: [["d", "w"], ["a"]].
     """
 
     # Check that input is correct
-    assert isinstance(text, list), "Input `text` is not a list."
+    _basic_assertions(text)
     assert (all(isinstance(sent, str) for sent in text)), "List element is not a string."
 
     if lower and remove_punctuation:
@@ -92,35 +110,33 @@ def _synset_lemmatizer(word):
         return word
 
 
-def lemmatize(tokens, lemmatizer="synset"):
+def lemmatize(corpus, lemmatizer="synset"):
     # TODO: add more lemmatizers
     """
-    Take a list of lists with tokens and return it in the same format with each word lemmatized.
-    There are various lemmatizers available, passed as string arguments:
-    - 'wordnet': Uses nltk.WordNetLemmatizer
-    - 'synset': Uses a custom implementation. Takes a word and returns its most common (or else simplest) synset.
+    Take a corpus and return it in the same format with each word lemmatized.
 
-    :param tokens:
-    :param lemmatizer:
-    :return:
+    :param tokens: Should be a list of lists with strings, e.g.: [["d", "w"], ["a"]].
+    :param lemmatizer: There are various lemmatizers available, passed as string arguments:
+        - 'wordnet': Uses nltk.WordNetLemmatizer
+        - 'synset': Uses a custom implementation. Takes a word and returns its most common (or else simplest) synset.
+    :return: List of lists with lemmatized tokens,  e.g.: [["d", "w"], ["a"]].
     """
 
     # Check that input is correct
-    assert isinstance(tokens, list), "Input `text` is not a list."
-    assert (all(isinstance(sent, list) for sent in tokens)), "Sentences are not lists."
-    assert (all(isinstance(token, str) for sent in tokens for token in sent)), "Tokens are not strings."
+    _basic_assertions(corpus, advanced_assertions=True)
 
     if lemmatizer == "wordnet":
         wnl = WordNetLemmatizer()
-        lemmatized_tokens = [[wnl.lemmatize(word) for word in sent] for sent in tokens]
+        lemmatized_tokens = [[wnl.lemmatize(word) for word in sent] for sent in corpus]
     elif lemmatizer == "synset":
-        lemmatized_tokens = [[_synset_lemmatizer(word) for word in sent] for sent in tokens]
+        lemmatized_tokens = [[_synset_lemmatizer(word) for word in sent] for sent in corpus]
     else:
         # return 'synset'
         print("Lemmatizer not recognized, using 'synset'.")
-        lemmatized_tokens = [[_synset_lemmatizer(word) for word in sent] for sent in tokens]
+        lemmatized_tokens = [[_synset_lemmatizer(word) for word in sent] for sent in corpus]
 
     return lemmatized_tokens
+
 
 def synset_lemmatization_mappings(original, lemmatized):
     """
@@ -128,14 +144,12 @@ def synset_lemmatization_mappings(original, lemmatized):
     the keys are the words in the lemmatized text and the values are lists of words from the original text
     that were transformed to said new word.
 
-    :param original: List of lists with string tokens.
-    :param lemmatized: List of lists with string tokens.
-    :return:
+    :param original: List of lists with string tokens, e.g.: [["d", "w"], ["a"]].
+    :param lemmatized: List of lists with string tokens, e.g.: [["d", "w"], ["a"]].
+    :return: A defaultdict of word mappings, e.g.: {"new":["old1", "old2"]}.
     """
-    for tokens in [original, lemmatized]:
-        assert isinstance(tokens, list), "Input `text` is not a list."
-        assert (all(isinstance(sent, list) for sent in tokens)), "Sentences are not lists."
-        assert (all(isinstance(token, str) for sent in tokens for token in sent)), "Tokens are not strings."
+    for corpus in [original, lemmatized]:
+        _basic_assertions(corpus, advanced_assertions=True)
 
     word_mappings = defaultdict(list)
 
@@ -144,3 +158,49 @@ def synset_lemmatization_mappings(original, lemmatized):
 
         if old not in word_mappings[new]:
             word_mappings[new].append(old)
+
+    return word_mappings
+
+# TODO: Maybe convert to class (the whole file?) since it will have a better interface and less programming headache
+def get_topics(text, algorithm="lda", no_features=1500, no_topics=30, no_top_words=10):
+    """
+    Take a list of sentences / documents and return a list of topics, and the created model.
+
+    :param text: A list of strings / sentences / documents.
+    :param no_topics: How many topics do we want to keep track of, int, default=30.
+    :param no_features: How many words we want to keep track of, int, default=1000.
+    :param no_top_words: How many words to keep track of per topic, int, default=10.
+    :return: A list of lists with keywords representing a topic, and the model instance.
+    """
+
+    # TODO: add other ways for topic modelling
+    _basic_assertions(text)
+    assert (all(isinstance(sent, str) for sent in text)), "List element is not a string."
+
+    if algorithm == "lda":
+
+        tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
+                                        max_features=no_features,
+                                        stop_words='english')
+
+        # Transform text into sparse counts
+        tf = tf_vectorizer.fit_transform(text)
+        # Keep track of original words
+        tf_feature_names = tf_vectorizer.get_feature_names()
+
+        # Create LDA model and fit it
+        lda = LatentDirichletAllocation(n_components=no_topics, max_iter=30,
+                                        learning_method='batch', evaluate_every=128,
+                                        perp_tol=1e-3, learning_offset=50.).fit(tf)
+
+        topics = [[tf_feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]] for topic in lda.components_]
+
+        return topics, lda
+
+
+
+def statistics(corpus):
+    # TODO: add more
+    frequency_dict = FreqDist([word for sent in corpus for word in sent])
+
+    return frequency_dict
